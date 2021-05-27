5adca69994f763d92710b1bd8885f2f7db997296
Mark Crelley <markcrelley2010@googlemail.com>
2021-05-27 14:40:25 +0800

    created model and added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+# TODO: Write a seed to insert 100 posts in the databas
+10.times do
+  Article.create(title: Faker::Movies::Ghostbusters.character, content: Faker::Movies::Ghostbusters.quote)
+end
