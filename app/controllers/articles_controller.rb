352330b27334e6f08c5ca096e5fb311a28dddbc7
Mark Crelley <markcrelley2010@googlemail.com>
2021-05-27 21:32:48 +0800

    new tasks now appear at top, all done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 	before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@articles = Article.all
+		@articles = Article.all.reverse
 	end
 
 	def new
